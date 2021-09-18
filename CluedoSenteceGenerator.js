dc4e5acff286cfb95bd4bcf94370672e933b24ee
Lorcan Mc <lorcan.jmc@gmail.com>
2021-09-18 17:09:45 +0100

    Built a simple function that outputs who dun it

--- CluedoSenteceGenerator.js
+++ CluedoSenteceGenerator.js
@@ -1,10 +1,14 @@
-const killers = [];
-const weapons = [];
-const rooms = [];
+const killers = ['Tom', 'Dick', 'Harry']
+const weapons = ['the knife', 'a hatchet', 'an AK47', 'a shuriken', 'a rope']
+const rooms = ['the bathtub', 'the kitchen', 'the treehouse', 'the Marianas trench', 'an igloo']
 
 
 function whoDunIt() {
+let randomKiller = killers[Math.floor(Math.random()*killers.length)]
+let randomWeapon = weapons[Math.floor(Math.random()*weapons.length)]
+let randomRoom = rooms[Math.floor(Math.random()*rooms.length)]
 
+console.log(`The murderer was ${randomKiller} with ${randomWeapon} in ${randomRoom}!`)
 }
 
 whoDunIt();
